d864040f439a4e3dde99e1418066c77b5c4305a3
TakuyaKushiyama <kushiyama.t@jrcs.co.jp>
2022-01-03 10:02:55 +0900

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WorksComponent } from './works/works.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { BannerComponent } from './banner/banner.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutsiteComponent } from './aboutsite/aboutsite.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorksComponent,
+    HomeComponent,
+    HeaderComponent,
+    BannerComponent,
+    AboutComponent,
+    SkillsComponent,
+    ExperienceComponent,
+    FooterComponent,
+    AboutsiteComponent
   ],
   imports: [
     BrowserModule,
