0fed7c3ac74783d86e5c60d82f63494a84b38911
TakuyaKushiyama <kushiyama.t@jrcs.co.jp>
2022-01-04 15:45:49 +0900

    Sorting by id

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,12 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../service/httpClient.service'
 
+interface Experience {
+  _id: String,
+  date: String,
+  companyName: String,
+  title: String,
+  role: String,
+  description: String,
+  workDetail1: String,
+  workDetail2: String,
+  workDetail3: String,
+  workDetail4: String,
+  workDetail5: String,
+}
 
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
   styleUrls: ['./experience.component.scss']
 })
+
 export class ExperienceComponent implements OnInit {
 
   experiences : any
@@ -20,12 +34,20 @@ export class ExperienceComponent implements OnInit {
 
     experienceObservable.subscribe(
       (data) => {
-        this.experiences = data;
+        const experienceData : [Experience] = data
+        experienceData.sort(this.sortById)
+        this.experiences = experienceData;
       },
       (err) => {
         console.log("ERROR");
       }
     )
+  }
+
+  sortById(a: Experience, b: Experience) {
+    let id1 = a._id
+    let id2 = b._id
+    return a._id > b._id ? 1 : -1
   }
 
 }
